32ef648dd00a9c493fc175acb8d73fad6853be12
Takayuki Matsubara <takayuki.1229@gmail.com>
2019-09-09 22:41:52 +0900

    Updates

---
+++ component---src-pages-page-2-js-fc1f55e31ef5a33266aa.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{214:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(102),c=a(209),r=a(208);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-fc1f55e31ef5a33266aa.js.map
